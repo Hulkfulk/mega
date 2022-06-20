22aab0613339e366fcfc87bda029f7b635f257bd
sachin <sachinkumarsingh51110@gmail.com>
2022-06-20 07:54:24 +0545

    ok

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ def course():
 def price():
     return render_template("price.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")    
+
 if __name__=='__main__':
     app.run(debug=True)    
