c26eeed68db62137c1976535b0e4c8dd883e54f7
sachin <sachinkumarsingh51110@gmail.com>
2022-06-20 06:58:18 +0545

    improved

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app=Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/course')
+def course():
+    return render_template("courses.html")
+
+@app.route('/price')
+def price():
+    return render_template("price.html")
+
 if __name__=='__main__':
     app.run(debug=True)    
