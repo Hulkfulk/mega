10fa6093e7682ee141be8aeea44d6a0313deb31a
sachin <sachinkumarsingh51110@gmail.com>
2022-06-21 16:02:35 +0545

    signuppage added

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask,render_template
 from flask_caching import Cache
+from flask_wtf import Form
+from wtforms import StringField,IntegerField,EmailField,SubmitField,TextAreaField,PasswordField,validators
+
 config={
     "DEBUG":True,
     "CACHE_TYPE":"SimpleCache",
     "CACHE_DEFAULT_TIMEOUT":300
 }
+class SignupForm(Form):
+    name=StringField('Name',[validators.DataRequired(),validators.length(min=3,max=100)])
+    phone=IntegerField('phone',[validators.DataRequired(),validators.length(min=10,max=10)])
+    email=EmailField('Email',[validators.DataRequired(),validators.length(min=5,max=200)])
+    submit=SubmitField('Submit')
+    
 
 app=Flask(__name__)
 
 app.config.from_mapping(config)
 cache=Cache(app)
+
+
+@app.route('/signupform')
+def signupform():
+    form=SignupForm()
+    return render_template("signup.html",form=form)
 
 
 @app.route('/')
