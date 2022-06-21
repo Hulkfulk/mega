618031a3000aff2a01d39c7a48aaf150c0e464b5
sachin <sachinkumarsingh51110@gmail.com>
2022-06-21 10:59:40 +0545

    caching try

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask,render_template
+from flask_caching import Cache
+config={
+    "DEBUG":True,
+    "CACHE_TYPE":"SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT":300
+}
+
 app=Flask(__name__)
 
+app.config.from_mapping(config)
+cache=Cache(app)
+
+
 @app.route('/')
+@cache.cached(timeout=330)
 def home():
     return render_template("home.html")
 
 @app.route('/course')
+@cache.cached(timeout=300)
 def course():
     return render_template("courses.html")
 
 @app.route('/price')
+@cache.cached(timeout=300)
 def price():
     return render_template("price.html")
 
 @app.route('/about')
+@cache.cached(timeout=300)
 def about():
     return render_template("about.html")    
 
